abb1c6f4d32160b9aa04c2d324c1843e4e3745ac
Carlos Herrera <carlos.herrera.c@tecsup.edu.pe>
2024-05-14 13:13:40 -0500

    update to cantidad when array = o the default value = 1

--- src/database/models/comanda.model.js
+++ src/database/models/comanda.model.js
@@ -4,7 +4,16 @@ const comandaSchema = new mongoose.Schema({
     mozos: { type: mongoose.Schema.Types.ObjectId, ref:'mozos' },
     mesas: { type: mongoose.Schema.Types.ObjectId, ref:'mesas' },
     platos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'platos' }],
-    cantidades: [Number],
+    cantidades: {
+        type: [Number],
+        default: function() {
+            if (this.cantidades.length === 0) {
+                return new Array(this.platos.length).fill(1);
+            } else {
+                return this.cantidades;
+            }
+        }
+    },
     observaciones: String,
     status: { 
         type: String,
@@ -22,6 +31,14 @@ const comandaSchema = new mongoose.Schema({
             return new Date(isoDate);
         }
     }
+},{ setDefaultsOnInsert: true });
+
+
+comandaSchema.pre('save', function(next) {
+    if (this.isNew && !this.cantidades.length) {
+        this.cantidades = new Array(this.platos.length).fill(1);
+    }
+    next();
 });
 
 const comandaModel = mongoose.model('Comanda', comandaSchema);
